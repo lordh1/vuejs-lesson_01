2110e899339cb75d52cf2a7b842cc078172fae31
Unknown <lordh@wp.pl>
2019-04-18 17:06:38 +0200

    JS workshops: "Loops" - lesson

--- src/components/Loops/Loops.vue
+++ src/components/Loops/Loops.vue
@@ -7,15 +7,27 @@
   <h3>Loop For In</h3>
   <p v-for="(item, index) in loopForIn()" :key="'lfi'+index">{{index+1}}. {{item}}</p>
   <hr />
+  <h3>Loop For Of</h3>
+  <p v-for="(item, index) in loopForOf()" :key="'lfo'+index">{{index+1}}. {{item}}</p>
+  <hr />
   <h3>Loop For In With Continue</h3>
   <p v-for="(item, index) in loopForInWithContinue()" :key="'lfiwc'+index">{{index+1}}. {{item}}</p>
+  <hr />
+  <h3>Loop For In With Break</h3>
+  <p v-for="(item, index) in loopForWithBreak()" :key="'lfiwb'+index">{{index+1}}. {{item}}</p>
+  <hr />
+  <h3>Loop While</h3>
+  <p v-for="(item, index) in loopWhile()" :key="'lw'+index">{{index+1}}. {{item}}</p>
+  <hr />
+  <h3>Loop Do While</h3>
+  <p v-for="(item, index) in loopDoWhile()" :key="'ldw'+index">{{index+1}}. {{item}}</p>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'HelloWorld',
+  name: 'Loops',
   props: {
     msg: String
   },
@@ -23,6 +35,7 @@ export default {
     return {
       topics: [
         'functions - basics of basics',
+        'if...else',
         'empty statement',
         'break',
         'continue',
@@ -46,22 +59,71 @@ export default {
   methods: {
     loopForIn () {
       let items = [];
-      for(let key in this.participants) {
-        items.push(this.participants[key]);
+      for(let key in this.car) {
+        items.push(key + ": " + this.car[key]);
       }
       return items;
     },
     loopForInWithContinue () {
       let items = [];
       for(let key in this.participants) {
-        if(key === "2") continue;
-        items.push(this.participants[key]);
+        if(key === "2" || key === "4") continue;
+        else if(key === "3") continue;
+        else {
+          items.push(this.participants[key]);
+        }
       }
       return items;
-    }
+    },
+    loopEmptyStatement () {
+      for(let i = 0; i < this.participants.length; this.participants[i++]  = 'HaHa...');
+    },
+    loopForWithBreak() {
+      let items = [];
+      for(let key in this.participants) {
+        if(key === "3") break;
+        else {
+          items.push(this.participants[key]);
+        }
+      }
+      return items;
+    },
+    loopWhile() {
+      let i = 0;
+      let items = [];
+
+      while(i < 5) {
+        i++;
+        items.push(i)
+      }
+
+      return items;
+    },
+    loopDoWhile() {
+      let i = 0;
+      let items = [];
+
+      do {
+        i++;
+        items.push("Element " + i)
+      }
+      while(i < 0)
+
+      return items;
+    },
+    loopForOf () {
+      let items = [];
+      for(let kupa of this.participants) {
+        items.push(kupa);
+      }
+      return items;
+    },
   },
-  mounted() {
-    this.loopForIn();
+  beforeMount() {
+    /*Uncomment following code to execute empty statement loop before first rendering of the page content
+     You'll see what will happen ;) */
+
+    // this.loopEmptyStatement();
   }
 }
 </script>
