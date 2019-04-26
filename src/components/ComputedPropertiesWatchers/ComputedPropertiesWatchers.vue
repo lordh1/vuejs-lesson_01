789e2580491e14f96c6f22dc1a83a3783ea339b4
Unknown <lordh@wp.pl>
2019-04-26 18:09:31 +0200

    during workshop

--- src/components/ComputedPropertiesWatchers/ComputedPropertiesWatchers.vue
+++ src/components/ComputedPropertiesWatchers/ComputedPropertiesWatchers.vue
@@ -2,10 +2,22 @@
   <div>
     <h1>Computed Properties & Watchers</h1>
     <section>
+      <h2>1. Custom events</h2>
+      <Message @click="wasSubmited" />
+    </section>
+    <section>
       <h2>1. Form with computed property</h2>
       <input type="text" v-model="inputMessage">
       <button @click="submit()">Submit</button>
       <p>{{alertMessage}}</p>
+      <p v-if="badWord" class="badWord">Bad word</p>
+    </section>
+    <section>
+      <h2>1. Form with computed property</h2>
+      <input v-for="(item, key) in inputs" :key="key" type="text" v-model="inputs[key]">
+      <button @click="submit()">Submit</button>
+      <p>{{alertMessage}}</p>
+      <p v-if="badWord" class="badWord">Bad word</p>
     </section>
     <section>
       <h2>2. Time counter</h2>
@@ -17,15 +29,21 @@
 
 <script>
 import { setInterval, setTimeout } from 'timers';
+import Message from "./Message";
 
 export default {
   name: 'ComputedPropertiesWatchers',
+  components: {Message},
   data() {
     return {
        counter: 0,
        tens: [],
        submited: false,
-       inputMessage: ""
+       inputMessage: "",
+       badWord: false,
+       inputs: [
+         "", "", ""
+       ]
     }
   },
   methods: {
@@ -38,6 +56,9 @@ export default {
     submit() {
       this.inputMessage = "";
       this.submited = true;
+    },
+    wasSubmited(message) {
+      alert("Message was submited! " + message);
     }
   },
   mounted() {
@@ -52,10 +73,10 @@ export default {
   },
   watch: {
     counter(newValue, oldValue) {
-      if(newValue > oldValue && !(newValue % 10)) !this.tens.length ? this.tens[0] = 'First ten seconds passed...' : this.tens = [...this.tens, 'Another ten seconds passed...']  
+      if(newValue > oldValue && !(newValue % 10)) !this.tens.length ? this.tens[0] = 'First ten seconds passed...' : this.tens = [...this.tens, 'Another ten seconds passed...']
     },
     tens: function(newValue, oldValue) {
-      if(newValue > oldValue) if(this.tens.length >= 6) this.tens = ['Too much records... Resetting!']; 
+      if(newValue > oldValue) if(this.tens.length >= 6) this.tens = ['Too much records... Resetting!'];
     },
     submited(newValue, oldValue) {
       if(newValue && !oldValue && this.submited) {
@@ -63,6 +84,14 @@ export default {
           this.submited = false;
         }, 5000);
       }
+    },
+    inputMessage(newValue) {
+      if(newValue.includes("kurwa")) this.badWord = true;
+      else this.badWord = false;
+    },
+    inputs(newValue, oldValue) {
+      console.log(newValue);
+      console.log(oldValue);
     }
   }
 }
@@ -104,5 +133,9 @@ button {
   font-size: 20px;
   padding: 10px;
   margin: 10px;
+}
+
+.badWord {
+  color: red;
 }
 </style>
---
+++ src/components/ComputedPropertiesWatchers/Message.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h1>Message</h1>
+    <section>
+      <input type="text" v-model="customMessage">
+      <button @click="submit">Submit</button>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  data() {
+    return {
+      customMessage: ""
+    }
+  },
+  methods: {
+    submit(e) {
+      console.log(e);
+      this.$emit('click', this.customMessage)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  /*display: inline-block;*/
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+section {
+  margin: 20px;
+  border: 1px solid red;
+}
+
+input {
+  padding: 10px;
+  margin: 10px;
+  font-size: 20px;
+}
+
+button {
+  background-color: #0055BB;
+  color: #FFFFFF;
+  font-size: 20px;
+  padding: 10px;
+  margin: 10px;
+}
+
+.badWord {
+  color: red;
+}
+</style>
