f551a5810c50a18266c36ac61941ca458350c3df
Unknown <lordh@wp.pl>
2019-05-16 09:48:00 +0200

    preparations #2

--- src/store/global.js
+++ src/store/global.js
@@ -24,12 +24,16 @@ export default {
         method: 'GET'
       };
 
-      const request = await fetch("http://bookingcars.cypel.pl:8080/car/find", options);
-      const data = await request.json();
+      try {
+        const request = await fetch("http://bookingcars.cypel.pl:8080/car/find", options);
+        const data = await request.json();
+        console.log(data);
+        await context.commit('setCars', data);
+      }
+      catch (err) {
+        alert(err);
+      }
 
-      console.log(data);
-
-      await context.commit('setCars', data);
     }
   }
 };
